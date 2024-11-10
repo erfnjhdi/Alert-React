f3b2a664c8018e404c19bbc77ba041014602741d
Erfan <erfanjahedi81@gmail.com>
2024-11-10 00:51:31 -0500

    Updated server.js and vercel.json for deployment

--- server/server.js
+++ server/server.js
@@ -61,5 +61,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
