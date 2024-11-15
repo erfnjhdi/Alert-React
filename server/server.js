2538243a58a82d449f092f0d51d89ef89713eb33
Erfan <erfanjahedi81@gmail.com>
2024-11-15 17:03:53 -0500

    testing env change

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 dotenv.config();
 
 //connect to MongoDB
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
